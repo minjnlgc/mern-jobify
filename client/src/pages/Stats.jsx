d7eff5ed1eb1b9a6b773889efe7d8e7e55636825
minjnlgc <kunyuquemian@outlook.com>
2025-01-02 13:46:38 +0000

    first commit

---
+++ client/src/pages/Stats.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { ChartsContainer, StatsContainer } from "../components";
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const Stats = () => {
+  const { defaultStats, monthlyApplications } = useLoaderData();
+  console.log(defaultStats, monthlyApplications);
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplications?.length > 1 && (
+        <ChartsContainer data={monthlyApplications} />
+      )}
+    </>
+  );
+};
+
+export default Stats;
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import {
+  BadRequestError,
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
+
+export const authenticateUser = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("authentication invalid");
+
+  try {
+    const { userId, role } = verifyJWT(token);
+    const testUser = userId === "6772aa04eeb6a91060dc725c";
+    req.user = { userId, role, testUser };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+};
+
+export const authorizePermissions = (...roles) => {
+  console.log(roles);
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError("unauthorised to access this route");
+    }
+    next();
+  };
+};
+
+export const checkForTestUser = (req, res, next) => {
+  if (req.user.testUser) throw new BadRequestError("Demo User. Read only");
+  next();
+};
